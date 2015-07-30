6a704be465c8d1128f9e3a2e620dd8929a79e3e4
sam <chenxsan@gmail.com>
2015-07-30 16:10:59 +0800

    清理 week.js 的测试代码

--- test/week.js
+++ test/week.js
@@ -1,7 +1,6 @@
 /**
  * Created by sam on 7/29/15.
  */
-import assert from 'assert';
 import React from 'react/addons';
 import {expect} from 'chai';
 import Week from '../src/week';
@@ -11,23 +10,18 @@ describe('Week component', function() {
 	let shallowRenderer = TestUtils.createRenderer();
 	shallowRenderer.render(<Week days={[undefined, undefined, 1, 2, 3, 4, 5]} selectDay={function(){}} highlight={true} day={1}/>);
 	let week;
-	
-	before('return Week component', function() {
+	before('return Week component', () => {
 		week = shallowRenderer.getRenderOutput();
 	});
-	
-	it('should have tr as container', function() {
+	it('should have tr as container', () => {
 		expect(week.type).to.equal('tr');
 	});
-	
 	it('should have 7 td children', () => {
 		expect(week.props.children.length).to.equal(7);
 	});
-	
 	it('children should have td as container', () => {
 		expect(week.props.children[0].type).to.equal('td');
 	});
-	
 	it('children should have class "datePicker__day"', () => {
 		expect(week.props.children[0].props.className).to.contain('datePicker__day');
 	});
@@ -35,11 +29,9 @@ describe('Week component', function() {
 	it('children without date data should have class "datePicker__day--disabled"', () => {
 		expect(week.props.children[0].props.className).to.contain('datePicker__day--disabled');
 	});
-	
 	it('chidlren of default date should have class "datePicker__day--today"', () => {
 		expect(week.props.children[2].props.className).to.contain('datePicker__day--today');
 	});
-
 	it('chidlren besides default date should not have class "datePicker__day--today"', () => {
 		expect(week.props.children[3].props.className).to.not.contain('datePicker__day--today');
 	});
