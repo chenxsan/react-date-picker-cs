58d12fb15a2094034dd2e9639cc2eb40901836a4
sam <chenxsan@gmail.com>
2015-08-26 14:07:59 +0800

    Update 2015-08-26T06:07:43.921Z

--- bundle.js
+++ bundle.js
@@ -783,11 +783,6 @@ exports['default'] = _react2['default'].createClass({
 			disabled: false,
 			range: [2010, 2020],
 			locale: 'en',
-			onChange: function onChange(date) {
-				// i will give you the date
-				// you also can decide whether to hide the popup calendar by passing this.props.isCalendarShow
-			},
-			onFocusIn: function onFocusIn() {},
 			value: ''
 		};
 	},
@@ -795,16 +790,41 @@ exports['default'] = _react2['default'].createClass({
 		disabled: _react2['default'].PropTypes.bool,
 		locale: _react2['default'].PropTypes.string,
 		onChange: _react2['default'].PropTypes.func.isRequired,
-		onFocusIn: _react2['default'].PropTypes.func,
 		range: _react2['default'].PropTypes.arrayOf(_react2['default'].PropTypes.number),
 		value: _react2['default'].PropTypes.string
 	},
+	getInitialState: function getInitialState() {
+		return {
+			isCalendarShow: false
+		};
+	},
+	componentDidMount: function componentDidMount() {
+		document.addEventListener('click', this.documentClickHandler);
+	},
+	componentWillUnmount: function componentWillUnmount() {
+		document.removeEventListener('click', this.documentClickHandler);
+	},
+	documentClickHandler: function documentClickHandler() {
+		this.setState({
+			isCalendarShow: false
+		});
+	},
+	onClickDatePickerArea: function onClickDatePickerArea(e) {
+
+		// stop the click event
+		e.nativeEvent.stopImmediatePropagation();
+	},
 	onClickCalendar: function onClickCalendar(date) {
+		this.setState({
+			isCalendarShow: false
+		});
 		this.props.onChange(date);
 	},
 	selectToday: function selectToday() {
-		var today = this.getToday();
-		this.props.onChange(today);
+		this.setState({
+			isCalendarShow: false
+		});
+		this.props.onChange(this.getToday());
 	},
 	calender: function calender() {
 		return _react2['default'].createElement(_calendar2['default'], { onClickCalendar: this.onClickCalendar, date: this.props.value, selectToday: this.selectToday, range: this.props.range, locale: this.props.locale });
@@ -813,14 +833,16 @@ exports['default'] = _react2['default'].createClass({
 		if (this.props.disabled === true) {
 			return;
 		}
-		this.props.onFocusIn();
+		this.setState({
+			isCalendarShow: true
+		});
 	},
 	render: function render() {
 		return _react2['default'].createElement(
 			'div',
-			{ className: "datePicker" },
+			{ className: "datePicker", onClick: this.onClickDatePickerArea },
 			_react2['default'].createElement('input', { className: 'datePicker__input ' + (this.props.disabled === true ? 'datePicker__input--disabled' : ''), type: 'text', onFocus: this.focusIn, value: this.props.value, readOnly: true, disabled: this.props.disabled }),
-			this.props.isCalendarShow === false ? null : this.calender()
+			this.state.isCalendarShow === false ? null : this.calender()
 		);
 	}
 });
